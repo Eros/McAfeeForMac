211491f4a223caecc684125df14954a07824efcd
Eros <george.marr.cobc@gmail.com>
2018-04-14 12:24:42 +0100

    init

---
+++ port.py
@@ -0,0 +1,21 @@
+import math, random, time
+print "Welcome to McAfee"
+scan = raw_input("Would you like your computer to be scanned for shitty viruses and not find them?")
+
+if scan != "yes"
+	print "you ain't the boss of me, I'll do it anyway"
+else:
+	print "Hold yo horses"
+		time.sleep(1)
+	s = random.randint(1, 1000)
+if s >= 900
+	print "Someone paid us not to catch their shit"
+else:
+	ud = raw_input("Zoinks, you have a virus, please give us your SSN and all your bank details"
+	print "You have entered", ud
+	confirm = raw_input("Come back in like 20 minutes and I'll probably not be done")
+	finish = 2
+while finish != 1:
+	time.sleep(10)
+	finish = random.randint(1, 4)
+print "Cheers guvna, all done!"
